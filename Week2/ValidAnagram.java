5c4700cd73a6cd97b6703d77174210f44730aa04
zongzhang <jingchang_007@163.com>
2020-11-29 17:16:59 +0800

    有效字母异位词

---
+++ Week2/ValidAnagram.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+/**
+ * 有效的字母异位词
+ *
+ * @since 1.0
+ */
+public class ValidAnagram {
+
+    // 直接用现成方法
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+
+    // 哈希表
+    // 记录出现频次 第一个遍历加 第二个遍历减
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
