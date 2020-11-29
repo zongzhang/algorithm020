a6203cf34ecd0bd78ae352f64a0499e6e21129e8
zongzhang <jingchang_007@163.com>
2020-11-29 17:16:19 +0800

    两数之和

---
+++ Week2/TwoSum.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @since 1.0
+ */
+public class TwoSum {
+    //暴力枚举
+    public int[] twoSum(int[] nums, int target) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    list.add(i);
+                    list.add(j);
+                    break;
+                }
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    // 哈希表
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
