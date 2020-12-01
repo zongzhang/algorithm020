fe8a47829cd11fa12783d612320deabc1fadd764
zongzhang <jingchang_007@163.com>
2020-12-01 23:51:19 +0800

    移动零

---
+++ Week1/MoveZeroes.java
@@ -0,0 +1,28 @@
+/**
+ * @since 1.0
+ */
+public class MoveZeroes {
+
+    //1、暴力解法
+    //2、双指针
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+
+    }
+
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+
+}
