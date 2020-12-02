995654a87e386b3b9dfd23b7561d258d1171d41f
zongzhang <jingchang_007@163.com>
2020-12-02 17:43:21 +0800

    生成括号

---
+++ Week3/GenerateParentheses.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 括号生成
+ *
+ * @since 1.0
+ */
+public class GenerateParentheses {
+
+    public static void main(String[] args) {
+        GenerateParentheses g = new GenerateParentheses();
+        System.out.println(g.generateParenthesis(3));
+    }
+
+    //1。暴力，生成所有括号组合，再检查正确性
+    //2。递归
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+
+    public void backtrack(List<String> ans, String cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+        if (open < max) {
+            backtrack(ans, cur + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(ans, cur + ")", open, close + 1, max);
+        }
+    }
+
+
+}
