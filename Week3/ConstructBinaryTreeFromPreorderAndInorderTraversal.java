fee12c75f77957ade9a1ba7673029636d68c4700
zongzhang <jingchang_007@163.com>
2020-12-05 16:57:49 +0800

    从前序遍历和中序遍历构造二叉树

---
+++ Week3/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,48 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @since 1.0
+ */
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+
+
+    //迭代
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
