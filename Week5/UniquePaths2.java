f647003672a06d925b1620b1fe416e712576ad91
zongzhang <jingchang_007@163.com>
2020-12-21 00:22:47 +0800

    不同路径2

---
+++ Week5/UniquePaths2.java
@@ -0,0 +1,33 @@
+/**
+ * 不同路径2  no63
+ *
+ * @since 1.0
+ */
+public class UniquePaths2 {
+
+    // 转移方程  f(m,n)=f(m−1,n)+f(m,n−1)
+    // 有障碍物 f(m,n)=0
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) {
+            return 0;
+        }
+
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
