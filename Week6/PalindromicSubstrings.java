a8fb7d1dcbc797e9324254d435058c10b3b20c37
zongzhang <jingchang_007@163.com>
2020-12-25 21:17:37 +0800

    回文子串

---
+++ Week6/PalindromicSubstrings.java
@@ -0,0 +1,22 @@
+/**
+ * 回文子串 no 647
+ * @since 1.0
+ */
+public class PalindromicSubstrings {
+
+    public int countSubstrings(String s) {
+        // 动态规划法
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int ans = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
