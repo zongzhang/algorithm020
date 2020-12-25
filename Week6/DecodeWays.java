01102b3377e288365f59f2cb3258130c2be7e34a
zongzhang <jingchang_007@163.com>
2020-12-25 21:11:01 +0800

    解码方法

---
+++ Week6/DecodeWays.java
@@ -0,0 +1,31 @@
+/**
+ * 解码方法 no 91
+ *
+ * @since 1.0
+ */
+public class DecodeWays {
+
+    // dp[n] = dp[n] + dp[n-1] 一个
+    // dp[n] = dp[n] + dp[n-2] 两个
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.parseInt(s.substring(i - 1, i));
+            int second = Integer.parseInt(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+
+}
